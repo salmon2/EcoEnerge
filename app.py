457c9d1d3ff54c58b30bc25e99d3902c3b902cb4
salmon2 <dys04076@naver.com>
2021-09-15 14:17:32 +0900

    m

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from types import MethodType
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import jwt
 
 import datetime
 import hashlib
@@ -13,8 +14,72 @@ import json
 
 app = Flask(__name__)
 
+SECRET_KEY = 'SPARTA'
+
+
 client = MongoClient('localhost', 27017)
 db = client.EcoEnerge
+# 토큰 가져오기
+@app.route('/')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+
+        return render_template('index.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.execeptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+# 로그인
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password.give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+            'id': username_receive, 
+            # 로그인 24시간 유지
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm= 'HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 토큰을 찾지 못한다면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+# 회원가입
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,
+        "password": password_hash
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+# ID 중복확인
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    
+    return jsonify({'result': 'success', 'exists': exists})
 
 
 @app.route('/')
@@ -44,13 +109,8 @@ def init():
 
     return jsonify({'result': 'success', 'msg': "성공"})
 
-<<<<<<< HEAD
 @app.route('/chargeList', methods = ['GET'])
-=======
-
-@app.route('/api/chargeList', methods=['GET'])
->>>>>>> 4db5b92099e0c5ae1e31b40b851900f6761eadc2
-def get_charges():
+def chargeList():
     page = request.args.get('page', type=int, default=1)  # 페이지
     print("page", page)
 
@@ -86,66 +146,67 @@ def charge():
     charge = db.chargeList.find_one({'_id': ObjectId(chargeId)})
     charge['_id'] = str(charge['_id'])
     
-
-
+    
     reviewList = []
     row_reviewList = db.review.find({'chargeId': ObjectId(chargeId)})
     for review in row_reviewList:
         review['_id'] = str(review['_id'])
+        review['chargeId'] = str(review['chargeId'])
         reviewList.append(review)
 
 
     return render_template("dummy_charge.html", charge = charge, reviewList = reviewList)
 
-@app.route('/review', methods = ['POST'])
-def review_save():
 
-@app.route('/api/review', methods=['POST'])
+@app.route('/review', methods=['POST'])
 def review_save():
-    memberId = request.form["memberId"]
+    token_receive = request.cookies.get('mytoken')
     chargeId = request.form["chargeId"]
-
     rate = request.form["rate"]
     contents = request.form["contents"]
     like = request.form["like"]
 
-    doc = {
-        "memberId": memberId,
-        "chargeId": chargeId,
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
 
-        "rate": rate,
-        "contents": contents,
-        "like": like
-    }
+        doc = {
+            "memberId": user_info["_id"],
+            "chargeId": ObjectId(chargeId),
 
-    db.review.insert_one(doc)
+            "rate": rate,
+            "contents": contents,
+            "like": like
+        }
 
-    return jsonify({'result': 'success', 'msg': '리뷰 저장!'})
+        db.review.insert_one(doc)
 
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("charge", id = chargeId))
+
+    return redirect(url_for("login"))
 
 @app.route('/review/update', methods = ['POST'])
 def review_update():
     review_id = request.form["_id"]
-    review_id = review_id[10:-2]
-
+    charge_id = request.form["chargeId"]
     rate = request.form["rate"]
     contents = request.form["contents"]
     like = request.form["like"]
 
     db.review.update({"_id": ObjectId(review_id)}, {"$set": {
-        "rate": rate,
-        "contents": contents,
-        "like": like
-    }
-    }
-                     )
+                                                            "rate": rate,
+                                                            "contents": contents,
+                                                            "like": like
+                                                            }
+                                                        }
+                    )
 
-    return jsonify({'result': 'success', 'msg': '리뷰 수정 완료!'})
+    return redirect(url_for("charge", id = "charge_id"))
 
 @app.route('/review/delete', methods = ['POST'] )
 def review_delete():
     review_id = request.form["_id"]
-    review_id = review_id[10:-2]
 
     db.review.delete_one({"_id": ObjectId(review_id)})
 
